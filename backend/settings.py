76ae11dc19be10a8659a79408d21e6d1178402c4
Aries Chen <arieschen@Ariess-Air-2.lan>
2023-08-18 19:14:39 +0800

    submudule changes

--- backend/settings.py
+++ backend/settings.py
@@ -126,5 +126,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000', 
+     'http://localhost:5173', 
+     'https://localhost:5173', 
 ]
